af83c4e7a5e1a512d33f6d0317f3e557a664ca2d
Marcus Vinicius <marcus.editor77@gmail.com>
2022-04-25 17:18:08 -0300

    second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-alert("Hello")
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form")
+    const input = document.querySelector("#new-task-input")
+    const list_el = document.querySelector("#tasks")
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value;
+
+        if (!task) {
+            alert("Please fill out the task");
+        } else {
+            console.log("Success");
+        }
+    })
+})
